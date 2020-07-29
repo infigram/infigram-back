a7523a7cb4c48dd72461a3eb95ac543948baf25c
Juggernaut9 <xbhavyatomar@gmail.com>
2020-07-29 18:08:24 +0530

    add post model

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model('Post', postSchema)
+
+//To do - add imageUrl , connect with user model
