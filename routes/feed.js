1b0277983d7c14bffb079135f0a0b3477dd06719
Your Name <ushinigami97@gmail.com>
2020-09-06 03:44:22 +0200

    CRUD for post almost completed
    + CRUD: add public_id to post model, update delete old image if image change, delete post also delete image, delete all will delete all image.
    +Update: dependencies, add google-auth-library if needed and update dependencies
    + CI/CD: upload to google engine, add real mongo uri
    + TODO: need to improve the deleteAll method and deleteAll will only delete all by user id, clear public_id_cloudinary and change to query string at runtime

--- routes/feed.js
+++ routes/feed.js
@@ -1,27 +1,33 @@
-const feedController = require('../controllers/feed')
-const isAuth = require('../middleware/is-auth')
-const imageUpload = require('../middleware/imageUpload')
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
+const imageUpload = require('../middleware/imageUpload');
 
 
-const router = require('express').Router()
+const router = require('express').Router();
 
 //Get all the posts
-router.get('/posts', feedController.getPosts)
+router.get('/posts', feedController.findAll);
+
+
+//Get all the posts
+router.get('/posts/:id', feedController.findById);
 
 //Post
-router.post('/post',
-         isAuth,
+router.post('/posts',
+        isAuth,
         imageUpload.single('image'),
-        feedController.createPost)
+        feedController.create);
 
 
 //Put
-router.put('/posts', feedController.updatePostById)
+router.put('/posts', feedController.updateById);
 
 //Patch
-router.patch('/posts', feedController.updatePostById)
+router.patch('/posts', feedController.updateById);
+
+router.delete('/posts/:id', feedController.deleteById);
 
 //Delete all posts
-router.delete('/posts', feedController.deleteAllPosts)
+router.delete('/posts', feedController.deleteAll);
 
 module.exports = router
